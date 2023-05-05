c586a664d2e3b31619b84755907a3384f4247b8c
chitwan <kaurchitwan@.com>
2023-05-04 18:30:13 -0700

    homepage css

--- app/app.py
+++ app/app.py
@@ -179,8 +179,13 @@ def upload_file():
 
 @app.route('/songs')
 def songs():
-    all_songs = Song.query.all()
-    return render_template('songs.html', songs=all_songs)
+    all_albums = Album.query.all()
+    return render_template('songs.html', albums=all_albums)
+
+# @app.route('/songs')
+# def songs():
+#     all_songs = Song.query.all()
+#     return render_template('songs.html', songs=all_songs)
 
 
 @app.route('/add_album', methods=['GET', 'POST'])
