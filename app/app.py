4dad853cf09461fa27db7ca6179cba1b7b0fbcf9
mehrdad <mehrdad.ordobadi@gmail.com>
2023-05-08 20:17:30 -0700

    user can create playlists and add songs to playlists

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,6 +12,7 @@ from biography_form import BiographyForm
 import os
 import mutagen
 from AlbumForm import AlbumForm
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
@@ -181,7 +182,8 @@ def search():
 def artist_page(artist_id):
     artist = Artist.query.get_or_404(artist_id)
     albums = Album.query.filter_by(artist_id=artist_id).all()
-    return render_template('artist_page.html', artist=artist, albums=albums)
+    playlists = Playlist.query.filter_by(listener_id=current_user.id).all() if current_user.is_authenticated else []
+    return render_template('artist_page.html', artist=artist, albums=albums, playlists=playlists)
 
 
 
@@ -203,6 +205,62 @@ def artist_biography(artist_id):
         form.biography.data = artist.artist_biography
 
     return render_template('artist_biography.html', artist=artist, form=form)
+
+# @app.route('/create_playlist', methods=['POST'])
+# @login_required
+# def create_playlist():
+#     data = request.get_json()
+#     playlist_name = data.get('playlist_name', '').strip()
+
+#     if not playlist_name:
+#         flash('Playlist can not be empty')
+#         return 400
+
+#     new_playlist = Playlist(playlist_title=playlist_name, listener_id=current_user.id)
+#     db.session.add(new_playlist)
+#     db.session.commit()
+
+#     return jsonify({'success': True})
+
+@app.route('/get_playlists')
+@login_required
+def get_playlists():
+    playlists = Playlist.query.filter_by(listener_id=current_user.id).all()
+    return jsonify({'playlists': [{'id': p.playlist_id, 'title': p.playlist_title} for p in playlists]})
+
+@app.route("/add_to_playlist", methods=["POST"])
+@login_required
+def add_to_playlist():
+    song_id = request.form.get("song_id")
+    print(f'Received song_id: {song_id}')
+    playlist_id = request.form.get("playlist_id")
+    new_playlist_name = request.form.get("new_playlist_name")
+
+    if playlist_id == "create":
+        if not new_playlist_name:
+            flash('Playlist name cannot be empty.', 'danger')
+            return redirect(request.referrer)
+
+        # Add the current timestamp when creating a new playlist
+        new_playlist = Playlist(playlist_title=new_playlist_name, playlist_creation_date=datetime.now(), listener_id=current_user.id)
+        db.session.add(new_playlist)
+        db.session.commit()
+        playlist_id = new_playlist.playlist_id
+
+    existing_entry = PlaylistSong.query.filter_by(playlist_id=playlist_id, song_id=song_id).first()
+    if existing_entry:
+        flash('The song is already in the playlist.', 'danger')
+    else:
+        print(f'-------------*******playlist id: {playlist_id}, song id: {song_id}******---------')
+        new_entry = PlaylistSong(playlist_id=playlist_id, song_id=song_id)
+        db.session.add(new_entry)
+        db.session.commit()
+        flash('Song added to the playlist.', 'success')
+
+    return redirect(request.referrer)
+
+
+
 
 
 if __name__ == '__main__':
